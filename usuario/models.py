3e6cc67663ad0b48f3f0cc328287fa66e062d7c6
Max <maxxd814@gmail.com>
2024-09-23 11:41:42 -0500

    version 0.0.0.1

--- usuario/models.py
+++ usuario/models.py
@@ -3,146 +3,24 @@
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove` lines if you wish to allow Django to create, modify, and delete the table
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
-class ActivityLog(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey('Users', models.DO_NOTHING, blank=True, null=True)
-    project = models.ForeignKey('Projects', models.DO_NOTHING, blank=True, null=True)
-    activity = models.TextField(blank=True, null=True)
-    timestamp = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-
-        db_table = 'activity_log'
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
 
 
 class Collaborations(models.Model):
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey('Users', models.DO_NOTHING, blank=True, null=True)
-    project = models.ForeignKey('Projects', models.DO_NOTHING, blank=True, null=True)
+    project = models.ForeignKey('Projects', models.DO_NOTHING, db_column='project', blank=True, null=True)
     role = models.TextField(blank=True, null=True)
 
     class Meta:
-
+        managed = False
         db_table = 'collaborations'
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-
-        db_table = 'django_session'
 
 
 class Notifications(models.Model):
@@ -153,7 +31,7 @@ class Notifications(models.Model):
     created_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-
+        managed = False
         db_table = 'notifications'
 
 
@@ -161,20 +39,53 @@ class Projects(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    creator = models.ForeignKey('Users', models.DO_NOTHING, blank=True, null=True)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    status = models.TextField(blank=True, null=True)
+    project_type = models.TextField(blank=True, null=True)
+    priority = models.TextField(blank=True, null=True)
+    responsible = models.ForeignKey('Users', models.DO_NOTHING, db_column='responsible', blank=True, null=True)
+    detailed_description = models.TextField(blank=True, null=True)
+    expected_benefits = models.TextField(blank=True, null=True)
+    necessary_requirements = models.TextField(blank=True, null=True)
+    progress = models.IntegerField(blank=True, null=True)
 
     class Meta:
-
+        managed = False
         db_table = 'projects'
+
+
+class TagAssociations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    tag = models.ForeignKey('Tags', models.DO_NOTHING, blank=True, null=True)
+    project = models.ForeignKey(Projects, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tag_associations'
+
+
+class Tags(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.TextField()
+    search_date = models.DateTimeField(blank=True, null=True)
+    counter = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tags'
 
 
 class Users(models.Model):
     id = models.BigAutoField(primary_key=True)
-    first_name = models.TextField(blank=True, null=True)
-    last_name = models.TextField(blank=True, null=True)
-    name = models.TextField(blank=True, null=True)
-    domain = models.TextField(blank=True, null=True)
+    university = models.TextField(blank=True, null=True)
+    career = models.TextField(blank=True, null=True)
+    cycle = models.TextField(blank=True, null=True)
+    biography = models.TextField(blank=True, null=True)
+    photo = models.TextField(blank=True, null=True)
+    achievements = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-
+        managed = False
         db_table = 'users'
