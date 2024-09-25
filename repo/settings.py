d26ca4c7b5f5320e7edd652b54d8408a97492f9b
Anderson <maxxd814@gmail.com>
2024-09-24 22:22:40 -0500

    version 0.0.2

--- repo/settings.py
+++ repo/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
+# Import dj-database-url at the beginning of the file.
+import dj_database_url
 
 
 load_dotenv()
@@ -35,6 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = env.bool('DEBUG', default=False)
 DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
@@ -58,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -91,12 +95,20 @@ WSGI_APPLICATION = 'repo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL'),
+#         conn_max_age=600  # Opcional: controla el tiempo máximo de vida de la conexión a la base de datos
+#     )
+# }
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
-        conn_max_age=600  # Opcional: controla el tiempo máximo de vida de la conexión a la base de datos
+        conn_max_age=600  # Opcional
     )
 }
+
 
 
 # Password validation
@@ -133,7 +145,14 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
